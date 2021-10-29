a17eb791bed358c8a649b9843b725756dfa5b0b7
Niranjan Jayakar <nija@amazon.com>
2021-10-29 15:19:23 +0100

    core

--- packages/@aws-cdk/core/test/bundling.test.ts
+++ packages/@aws-cdk/core/test/bundling.test.ts
@@ -1,10 +1,12 @@
 import * as child_process from 'child_process';
 import * as crypto from 'crypto';
 import * as path from 'path';
+import { describeDeprecated } from '@aws-cdk/cdk-build-tools';
 import * as sinon from 'sinon';
-import { BundlingDockerImage, DockerImage, FileSystem } from '../lib';
+import { DockerImage, FileSystem } from '../lib';
 
-describe('bundling', () => {
+// describeDeprecated() is required because of this bug - https://github.com/aws/jsii/issues/3102
+describeDeprecated('bundling', () => {
   afterEach(() => {
     sinon.restore();
 
@@ -21,7 +23,7 @@ describe('bundling', () => {
       signal: null,
     });
 
-    const image = BundlingDockerImage.fromRegistry('alpine');
+    const image = DockerImage.fromRegistry('alpine');
     image.run({
       command: ['cool', 'command'],
       environment: {
@@ -136,7 +138,7 @@ describe('bundling', () => {
       error: new Error('UnknownError'),
     });
 
-    const image = BundlingDockerImage.fromRegistry('alpine');
+    const image = DockerImage.fromRegistry('alpine');
     expect(() => image.run()).toThrow(/UnknownError/);
 
   });
@@ -151,13 +153,13 @@ describe('bundling', () => {
       signal: null,
     });
 
-    const image = BundlingDockerImage.fromRegistry('alpine');
+    const image = DockerImage.fromRegistry('alpine');
     expect(() => image.run()).toThrow(/\[Status -1\]/);
 
   });
 
   test('BundlerDockerImage json is the bundler image name by default', () => {
-    const image = BundlingDockerImage.fromRegistry('alpine');
+    const image = DockerImage.fromRegistry('alpine');
 
     expect(image.toJSON()).toEqual('alpine');
 
@@ -199,7 +201,7 @@ describe('bundling', () => {
     });
 
     const imagePath = path.join(__dirname, 'fs/fixtures/test1');
-    BundlingDockerImage.fromAsset(imagePath, {
+    DockerImage.fromAsset(imagePath, {
       file: 'my-dockerfile',
     });
 
@@ -221,7 +223,7 @@ describe('bundling', () => {
     });
 
     const imagePath = path.join(__dirname, 'fs/fixtures/test1');
-    const image = BundlingDockerImage.fromAsset(imagePath, {
+    const image = DockerImage.fromAsset(imagePath, {
       file: 'my-dockerfile',
     });
     expect(image).toBeDefined();
@@ -240,7 +242,7 @@ describe('bundling', () => {
       signal: null,
     });
 
-    const image = BundlingDockerImage.fromRegistry('alpine');
+    const image = DockerImage.fromRegistry('alpine');
     image.run({
       entrypoint: ['/cool/entrypoint', '--cool-entrypoint-arg'],
       command: ['cool', 'command'],
@@ -281,7 +283,7 @@ describe('bundling', () => {
     });
 
     // WHEN
-    BundlingDockerImage.fromRegistry('alpine').cp('/foo/bar', '/baz');
+    DockerImage.fromRegistry('alpine').cp('/foo/bar', '/baz');
 
     // THEN
     expect(spawnSyncStub.calledWith(sinon.match.any, ['create', 'alpine'], sinon.match.any)).toEqual(true);
@@ -315,7 +317,7 @@ describe('bundling', () => {
 
     // WHEN
     expect(() => {
-      BundlingDockerImage.fromRegistry('alpine').cp('/foo/bar', '/baz');
+      DockerImage.fromRegistry('alpine').cp('/foo/bar', '/baz');
     }).toThrow(/Failed.*copy/i);
 
     // THEN
